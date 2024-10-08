d140950fc14c872f0806e3234ad096303bb69483
jfandre00 <jfandre@gmail.com>
2024-10-08 20:32:21 -0300

    add beecrowd 2915

--- Exercícios/DevTrio/Competicao Clube/beecrowd 2915.py
+++ Exercícios/DevTrio/Competicao Clube/beecrowd 2915.py
@@ -1,4 +1,4 @@
-def mount_marathon(n, cartas):
+'''def mount_marathon(n, cartas):
     pilhaDeCartas = cartas[:]  # Cópia da lista original
 
     while True:
@@ -24,4 +24,65 @@ def mount_marathon(n, cartas):
 n = int(input()) 
 cartas = list(map(int, input().split()))  
 
+print(mount_marathon(n, cartas))'''
+
+'''
+Primeiro exemplo 
+6 (cartas)
+5 8 6 6 10 4 
+
+podemos mover o 10 para a direita, para cima da carta 4
+
+5 8 6 6 4
+        10
+
+mover o 8 para cima do 6
+
+5  6  6  4
+   8    10
+
+nao consigo mais mover pois o 5 e 6 são menores que 8 e 10
+
+------ tentando de outra forma
+
+5 8 6 6 10 4
+
+5 8 6 6  4
+        10
+
+5 8 6  4
+    6  10
+
+5 6  4
+  6  10
+  8
+
+lógica - precisa ser maior ou igual e só move para a direita. Pilhas únicas são as que podem ser movidas
+segredo é fazer da direita pra esquerda.
+'''
+
+'''
+1. pilhas <- N
+2. Para p de 1 até N-1:
+   Se carta(p) >= carta(p+1):
+    pilhas <- pilhas - 1
+'''
+
+def mount_marathon(n, cartas):
+    pilhas = n
+
+    for p in range(n-1):
+        if cartas[p] >= cartas[p+1]:
+            pilhas -= 1
+
+    return pilhas
+
+n = int(input()) 
+cartas = list(map(int, input().split()))  
+
 print(mount_marathon(n, cartas))
+
+
+
+
+
